58045e8b1c9ff7a085a8fbb678cb7c0cca8eac99
Ayrton Salvador <salvador.ayrton@gmail.com>
2021-12-10 17:42:53 +0100

    Update Connect.js
    Added secret keys

--- BACK/models/Connect.js
+++ BACK/models/Connect.js
@@ -1,4 +1,6 @@
 var mongoose =require('mongoose')
+
+
  
 var options = {
    connectTimeoutMS: 5000,
@@ -6,7 +8,7 @@ var options = {
    useNewUrlParser: true,
 }
  
-mongoose.connect('mongodb+srv://Juliettefontaine:94Mongo.@cluster0.g9yct.mongodb.net/GlouBDD?retryWrites=true&w=majority',
+mongoose.connect(`mongodb+srv://${USERNAME}:${PASSWORD}.@cluster0.g9yct.mongodb.net/GlouBDD?retryWrites=true&w=majority`,
 options,
 function(err){
    console.log(err);
